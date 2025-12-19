4504903e3a47cf33b3a6d7bdcaa2baaea02a774a
Rodgers Ndocha <56960174+RodgersNdocha@users.noreply.github.com>
2025-12-19 12:44:46 +0200

    Update extract.py

--- extract.py
+++ extract.py
@@ -45,9 +45,10 @@ def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
     print(f'File {source_file_name} uploaded to {destination_blob_name} in {bucket_name}.')
 
 # Set your GCS bucket name and destination file name
-bucket_name = 'capetown-1'
+bucket_name = 'devfest-kigali'
 source_file_name = 'employee_data.csv'
 destination_blob_name = 'employee_data.csv'
 
 # Upload the CSV file to GCS
 upload_to_gcs(bucket_name, source_file_name, destination_blob_name)
+
